681af7d7873db4cdd2c0014abfbb7b6916afe4c7
SHIVA6174 <psivaiah6174@gmail.com>
2025-06-06 11:27:50 +0530

    Route Params(:id)

--- src/App.js
+++ src/App.js
@@ -21,4 +21,26 @@ app.get("/api/products", (req, res) => {
   res.status(201).send({ Products: Products });
 });
 
+// Route Params:
+
+app.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(`ID: ${id}`);
+  console.log(`Type of ID: ${typeof id}`);
+
+  const parsedID = parseInt(id);
+  console.log(`ID: ${parsedID}`);
+  console.log(`Type of ID: ${typeof parsedID}`);
+
+  if (isNaN(parsedID))
+    return res.status(400).send({ msg: "Bad Request!(Invalid ID)" });
+
+  const findUser = Users.find((user) => user.id === parsedID);
+  console.log(findUser);
+
+  if (!findUser) return res.sendStatus(404);
+
+  return res.status(200).send(findUser);
+});
+
 app.listen(port, () => `Server running on port: ${port} 🔥`);
