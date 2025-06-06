5d3944c0306240337d5bc09ec5688c7565b98f9b
SHIVA6174 <psivaiah6174@gmail.com>
2025-06-06 10:49:25 +0530

    GET Requests

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import Users from "./UsersData.js";
+import Products from "./ProductsData.js";
 
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 9000;
 
+// GET Requests:
+
 app.get("/", (req, res) => {
   res.send("<h1>Home Page!</h1>");
+});
+
+app.get("/api/users", (req, res) => {
+  res.status(200).send({ Users: Users });
+});
+
+app.get("/api/products", (req, res) => {
+  res.status(201).send({ Products: Products });
 });
 
 app.listen(port, () => `Server running on port: ${port} 🔥`);
